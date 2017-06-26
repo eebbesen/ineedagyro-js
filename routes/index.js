a28431fba4187e7efb0bc87e07cd10f85ef20838
J2EEbbesen <eebbesen@users.noreply.github.com>
2017-06-26 13:29:49 -0500

    Yelp (#1)
    Adding Yelp

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express')
 var router = express.Router()
 
+const Yelp = require('node-yelp-api-v3')
+const yelp = new Yelp({
+  consumer_key: process.env.YELP_KEY,
+  consumer_secret: process.env.YELP_SECRET
+})
+const SEARCH = {
+  term: 'gyro',
+  location: 55104,
+  radius: 2500
+}
+
 router.get('/', function(req, res){
-  res.render('index')
+  yelp.searchBusiness(SEARCH)
+    .then((results) => {
+      res.render('index', { locs: results.businesses })
+    })
 })
 
 module.exports = router
