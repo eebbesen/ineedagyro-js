fb065f9ffa90df941e4598852b8af62a24775257
J2EEbbesen <eebbesen@users.noreply.github.com>
2017-06-26 20:33:24 -0500

    Displaying locations (#2)
    Displaying locations

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,22 @@ const yelp = new Yelp({
   consumer_key: process.env.YELP_KEY,
   consumer_secret: process.env.YELP_SECRET
 })
-const SEARCH = {
-  term: 'gyro',
-  location: 55104,
-  radius: 2500
-}
+
+router.get('/recs', function(req, res){
+  s = {
+    term: 'gyro',
+    latitude: req.query.lat,
+    longitude: req.query.lng,
+    radius: 2000
+  }
+  yelp.searchBusiness(s)
+    .then((results) => {
+      res.send({ locs: results.businesses })
+    })
+})
 
 router.get('/', function(req, res){
-  yelp.searchBusiness(SEARCH)
-    .then((results) => {
-      res.render('index', { locs: results.businesses })
-    })
+  res.render('index', { locs: [] })
 })
 
 module.exports = router
