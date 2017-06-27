6b594b1b2f57bb2dfda64e20a8564a5d485f6769
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-06-26 20:32:49 -0500

    Removing stuff

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,6 @@ const yelp = new Yelp({
   consumer_key: process.env.YELP_KEY,
   consumer_secret: process.env.YELP_SECRET
 })
-const SEARCH = {
-  term: 'gyro',
-  location: 55104,
-  radius: 2500
-}
 
 router.get('/recs', function(req, res){
   s = {
@@ -26,10 +21,6 @@ router.get('/recs', function(req, res){
 })
 
 router.get('/', function(req, res){
-  // yelp.searchBusiness(SEARCH)
-  //   .then((results) => {
-  //     res.render('index', { locs: results.businesses })
-  //   })
   res.render('index', { locs: [] })
 })
 
