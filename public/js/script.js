ad396a1639ba78c2a6f35a33da6564148b0392a8
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-06-28 06:02:57 -0500

    Refactor for testing

--- public/js/script.js
+++ public/js/script.js
@@ -3,17 +3,24 @@ function formatResults(results) {
   const json = JSON.parse(results)
   var str = ''
   json.locs.map((loc) => {
-    str += '<div class="record"><a href="'+ loc.url + '">' + loc.name + '</a> ' + loc.location.address1 + '</div>'
+    str += '<div class="record"><a href="'
+        +  loc.url + '">' + loc.name + '</a> '
+        +  loc.location.address1 + '</div>'
   })
 
-  document.querySelector('#results').innerHTML = str
+  return str
 }
 
-function getLocation() {
+function showLocations(html) {
+  document.querySelector('#results').innerHTML = html
+}
+
+function populateResults() {
   navigator.geolocation.getCurrentPosition(function(location) {
     const xhr = new XMLHttpRequest()
     xhr.onload = function () {
-      formatResults(xhr.response)
+      const html = formatResults(xhr.response)
+      showLocations(html)
     }
     xhr.open("GET", window.location + 'recs?lat=' + location.coords.latitude + '&lng=' + location.coords.longitude)
     xhr.send()
