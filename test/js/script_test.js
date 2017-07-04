57ac79f096b9b278b26676453c17826848dab41e
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-07-04 08:02:22 -0500

    Adding message when no locations found in range

--- test/js/script_test.js
+++ test/js/script_test.js
@@ -15,14 +15,14 @@ describe('formatResults', () => {
   describe('has no locs', () => {
     it ('retuns an empty string', () => {
       const ret = formatResults('{"locs":[]}')
-      expect(ret).toEqual('')
+      expect(ret).toEqual('<div>No results found near you -- συγνώμη!</div>')
     })
   })
 
   describe('has no json', () => {
     it ('retuns an empty string', () => {
       const ret = formatResults('{}')
-      expect(ret).toEqual('')
+      expect(ret).toEqual('<div>No results found near you -- συγνώμη!</div>')
     })
   })
 })
