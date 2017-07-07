d2ad804ce3016bde96df70abfbd51c634bad8879
Eric Ebbesen <eebbesen@eebbesen-mbp.hitronhub.home>
2017-07-07 13:38:52 -0500

    Adding distance to location and flagging food trucks

--- test/js/script_test.js
+++ test/js/script_test.js
@@ -1,14 +1,16 @@
 describe('formatResults', () => {
   describe('has records', () => {
     const json = {"locs":[
-                   {"url":"https://fake.business.biz/1","name":"Hero Gyros","location":{"address1":"1600 Grand Ave"}},
-                   {"url":"https://fake.business.biz/2","name":"Russkaya Shaverma","location":{"address1":"900 University Ave"}}
+                   {"url":"https://fake.business.biz/1","name":"Hero Gyros","location":{"address1":"1600 Grand Ave"},"distance":1003.12},
+                   {"url":"https://fake.business.biz/2","name":"Russkaya Shaverma","location":{"address1":"900 University Ave"},"distance":88.67},
+                   {"url":"https://fake.business.biz/3","name":"Gyro Truck","location":{"address1":""},"distance":""}
                   ]}
 
     it ('retuns a string of results', () => {
       const ret = formatResults(json)
-      expect(ret).toEqual('<div class="record"><a href="https://fake.business.biz/1">Hero Gyros</a> 1600 Grand Ave</div>'
-                        + '<div class="record"><a href="https://fake.business.biz/2">Russkaya Shaverma</a> 900 University Ave</div>')
+      expect(ret).toEqual('<div class="record"><a href="https://fake.business.biz/1">Hero Gyros</a> 1600 Grand Ave (1003 meters)</div>'
+                        + '<div class="record"><a href="https://fake.business.biz/2">Russkaya Shaverma</a> 900 University Ave (88 meters)</div>'
+                        + '<div class="record"><a href="https://fake.business.biz/3">Gyro Truck</a> (food truck)</div>')
     })
   })
 
