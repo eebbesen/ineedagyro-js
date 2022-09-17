bface7a0054f6cf48522b290e511da723d543497
J2EEbbesen <eebbesen@users.noreply.github.com>
2022-09-17 10:33:09 -0500

    updatng node version and npm audit fixes (#82)
    * updatng node version and npm audit fixes
    
    * updating circleci

--- test/routes/index_test.js
+++ test/routes/index_test.js
@@ -11,7 +11,7 @@ let stub
 
 describe('router', function(){
   beforeEach(function(){
-    sandbox = sinon.sandbox.create()
+    sandbox = sinon.createSandbox()
     stub = sandbox.stub(Yelp, 'client')
   })
 
